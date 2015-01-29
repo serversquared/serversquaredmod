3c4c005bb26961b49d5bf15c2e596686c9b871be
account3r2 <ilove1000bananas@gmail.com>
2015-01-28 18:41:11 -0800

    Fixed URL variable in getFromServer().

--- lua/scripts/serversquaredmodificationcore.lua
+++ lua/scripts/serversquaredmodificationcore.lua
@@ -16,7 +16,7 @@ Written by server <serversquaredmain@gmail.com>, January 2015.
 SSCore = {}
 SSCore.baseversionCore = "9"			-- Base version of the Core.
 SSCore.baseversionAPI = "1"				-- Base version of the Core API. Modules should probably work if they were written for this base version.
-SSCore.versionCore = "9.0.0"			-- Version of the Core.
+SSCore.versionCore = "8.0.0"			-- Version of the Core.
 SSCore.versionAPI = "1.0"				-- Version of the Core API.
 SSCore.alpha = false					-- True if this build is an alpha build.
 SSCore.beta = true						-- True if this build is a beta build.
@@ -452,7 +452,7 @@ function SSCore.getFromServer(file)
 	SSCore.log("Setting host.", 0, "Server Core")
 	local host = SSCore.http .. (file or blank)
 	SSCore.log("Testing connection.", 0, "Server Core")
-	local data, reply, head = http.request(SSCore.url .. "/test.html")
+	local data, reply, head = http.request(SSCore.http .. "/test.html")
 	if reply == 200 and data == blank then
 		SSCore.log("Sending request: GET " .. (file or blank), 0, "Server Core")
 		local data, reply, head = http.request(host)
