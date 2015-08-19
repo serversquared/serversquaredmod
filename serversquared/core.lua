a4267629518e856029377f2568fa1c6230faae5d
Niko Geil <code@serversquared.org>
2015-08-18 18:30:20 -0700

    Add core source file.
    This file will eventually contain the code that allows the API to load in new
    modules and start up properly. It will be required to use the Modification.

---
+++ serversquared/core.lua
@@ -0,0 +1,22 @@
+--[[=========================================================================\\
+  || This file is part of (server)² Modification, an easily extendible       ||
+  || modification for AssaultCube.                                           ||
+  || Copyright (C) 2011-2015 Niko Geil.                                      ||
+  ||                                                                         ||
+  || (server)² Modification is free software: you can redistribute it and/or ||
+  || modify it under the terms of the GNU Affero General Public License as   ||
+  || published by the Free Software Foundation, either version 3 of the      ||
+  || License, or (at your option) any later version.                         ||
+  ||                                                                         ||
+  || (server)² Modification is distributed in the hope that it will be       ||
+  || useful, but WITHOUT ANY WARRANTY; without even the implied warranty of  ||
+  || MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero ||
+  || General Public License for more details.                                ||
+  ||                                                                         ||
+  || You should have received a copy of the GNU Affero General Public        ||
+  || License along with (server)² Modification. If not, see                  ||
+  || <http://www.gnu.org/licenses/>.                                         ||
+  \\=========================================================================]]
+
+-- Main table for (server)² Modification.
+local SS = {}
